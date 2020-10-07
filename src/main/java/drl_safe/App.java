60f174131b176269687d9f384bb5350044996d71
Jason Morris <jason@roundtablelaw.ca>
2020-10-07 15:58:29 -0600

    adding remaining rules and agenda groups

--- src/main/java/drl_safe/App.java
+++ src/main/java/drl_safe/App.java
@@ -1,8 +1,13 @@
 package drl_safe;
 
 import org.kie.api.KieServices;
+import org.kie.api.event.rule.DebugAgendaEventListener;
+import org.kie.api.event.rule.DebugRuleRuntimeEventListener;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.Agenda;
+
+import java.util.Collection;
 
 
 public class App 
@@ -16,8 +21,32 @@ public class App
         // Create a session from the drl_safe specification in the kmodule.XML file.
         KieSession ksession = kc.newKieSession("drl_safe_KS");
 
-        // and fire the rules
+        //ksession.addEventListener( new DebugRuleRuntimeEventListener() );
+        
+        //ksession.addEventListener( new DebugAgendaEventListener() );
+
+      
+        Agenda agenda = ksession.getAgenda();
+        agenda.getAgendaGroup( "start" ).setFocus();
         ksession.fireAllRules();
+        agenda.getAgendaGroup( "capitalization" ).setFocus();
+        ksession.fireAllRules();
+        agenda.getAgendaGroup( "prices 1" ).setFocus();
+        ksession.fireAllRules();
+        agenda.getAgendaGroup( "prices 2" ).setFocus();
+        ksession.fireAllRules();
+        agenda.getAgendaGroup( "pre-terminate" ).setFocus();
+        ksession.fireAllRules();
+        agenda.getAgendaGroup( "terminate" ).setFocus();
+        ksession.fireAllRules();
+        agenda.getAgendaGroup( "post-terminate" ).setFocus();
+        ksession.fireAllRules();
+      
+        // and fire the rules
+        // ksession.fireAllRules();
+
+        // Put the resulting data in a variable for debugging purposes.
+        Collection<? extends Object> objects = ksession.getObjects();
 
         // and then dispose the session
         ksession.dispose();
